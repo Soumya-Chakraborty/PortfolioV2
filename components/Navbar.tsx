318a289eee5b7748a8dfd3728439b9ccb5b07b4b
Soumya Chakraborty <soumyachakraborty198181@gmail.com>
2023-12-28 14:49:55 +0530

    Navbar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -301,7 +301,7 @@ const Navbar = () => {
                 className="text-sm w-72 tracking-widest text-textGreen text-center mt-4"
                 href="soumyachakraborty198181@gmail.com"
               >
-                <p>reactbd@proton.me</p>
+                <p>soumyachakraborty198181@gmail.com</p>
               </motion.a>
             </motion.div>
           </div>
